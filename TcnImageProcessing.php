dbeca37d01291884c11be5256ab2d0ea13536aa1
Paul J. Morris <mole@morris.net>
2013-03-29 18:20:33 -0400

    ISSUE: NEVP Node:129 PURPOSE: Allow removal of ingested XML files. DESCRIPTION: Copied block from csv import section to remove or move processed xml files.

--- TcnImageProcessing.php
+++ TcnImageProcessing.php
@@ -249,7 +249,7 @@ class SpecProcessorManager {
 
 	private function processFolder($pathFrag = ''){
 		set_time_limit(2000);
-		$this->logOrEcho("Processing: ".$this->sourcePathBase.$pathFrag." \n");
+		//$this->logOrEcho("Processing: ".$this->sourcePathBase.$pathFrag." \n");
 		//Read file and loop through images
 		if(!file_exists($this->sourcePathBase.$pathFrag)){
 			$this->logOrEcho("\tSource path does not exist: ".$this->sourcePathBase.$pathFrag." \n");
@@ -357,7 +357,21 @@ class SpecProcessorManager {
 						 }
 					}
 					if ($foundSchema) { 
-						 $this->logOrEcho('Processed: '.$pathFrag.$fileName."\n");
+						$this->logOrEcho('Processed: '.$pathFrag.$fileName."\n");
+						if($this->keepOrig){
+							$fileName = substr($filePath,strrpos($filePath,'/')).'.orig_'.time();
+							if(!file_exists($this->targetPathBase.$this->targetPathFrag.'orig_xml')){
+								mkdir($this->targetPathBase.$this->targetPathFrag.'orig_xml');
+							}
+							if(!rename($filePath,$this->targetPathBase.$this->targetPathFrag.'orig_xml'.$fileName)){
+								$this->logOrEcho("\tERROR: unable to move (".$filePath.") \n");
+							}
+						 } else {
+							if(!unlink($filePath)){
+								$this->logOrEcho("\tERROR: unable to delete file (".$filePath.") \n");
+							}
+						}
+
 					} 
 					else { 
 						 $this->logOrEcho("\tERROR: Unable to match ".$pathFrag.$fileName." to a known schema.\n");
